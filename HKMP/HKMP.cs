cb2a3c011d596bc1a13998558799632f13cbe5a2
Extremelyd1 <Extremelyd1@outlook.com>
2021-03-18 19:47:27 +0100

    Bumped version number

--- HKMP/HKMP.cs
+++ HKMP/HKMP.cs
@@ -13,7 +13,7 @@ namespace HKMP {
         private ModSettings _modSettings = new ModSettings();
 
         public override string GetVersion() {
-            return "0.2.0";
+            return "0.2.1";
         }
 
         public override List<(string, string)> GetPreloadNames() {
