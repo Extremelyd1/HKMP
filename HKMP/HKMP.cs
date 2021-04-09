b20b5b477fdd7eeb4d2111d1894c3ac44d1f478b
Prashant Mohta <prashant.mohta@testbook.com>
2021-04-09 11:13:04 +0530

    bump version text

--- HKMP/HKMP.cs
+++ HKMP/HKMP.cs
@@ -13,7 +13,7 @@ namespace HKMP {
         private ModSettings _modSettings = new ModSettings();
 
         public override string GetVersion() {
-            return "0.4.0 - ServerKnights v0.4";
+            return "0.4.0 - ServerKnights v0.5";
         }
 
         public override List<(string, string)> GetPreloadNames() {
