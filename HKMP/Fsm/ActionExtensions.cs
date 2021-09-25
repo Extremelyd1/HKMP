4c7ab8e64cd745d12a0fff6229f7b09dd8be29bb
Extremelyd1 <Extremelyd1@outlook.com>
2021-09-25 14:22:35 +0200

    Remove debug message and implement two new actions

--- HKMP/Fsm/ActionExtensions.cs
+++ HKMP/Fsm/ActionExtensions.cs
@@ -17,8 +17,6 @@ namespace Hkmp.Fsm {
         private static void ExecuteAction(FsmStateAction action) {
             var type = action.GetType();
             
-            Logger.Get().Info("ActionExtensions", $"ExecuteAction on type: {type}");
-
             var methodInfo = typeof(ActionExtensions).GetMethod("Execute", new [] {type});
             if (methodInfo == null) {
                 Logger.Get().Warn("ActionExtensions", $"Could not find Execute method for type: {type}");
@@ -741,6 +739,26 @@ namespace Hkmp.Fsm {
         public static void Execute(this FloatOperator instance) {
             typeof(FloatOperator).InvokeMember(
                 "DoFloatOperator",
+                BindingFlags,
+                null,
+                instance,
+                null
+            );
+        }
+
+        public static void Execute(this SetAudioPitch instance) {
+            typeof(SetAudioPitch).InvokeMember(
+                "DoSetAudioPitch",
+                BindingFlags,
+                null,
+                instance,
+                null
+            );
+        }
+
+        public static void Execute(this AudioPlayRandom instance) {
+            typeof(AudioPlayRandom).InvokeMember(
+                "DoPlayRandomClip",
                 BindingFlags,
                 null,
                 instance,
