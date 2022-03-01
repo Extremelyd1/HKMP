f686e2f3c31ee4b726ab105b62b55270ffe7bc28
Extremelyd1 <Extremelyd1@outlook.com>
2022-03-01 14:51:56 +0100

    Add command interfaces and client command manager

---
+++ HKMP/Api/Command/Client/IClientCommand.cs
@@ -0,0 +1,13 @@
+namespace Hkmp.Api.Command.Client {
+    /// <summary>
+    /// Interface for client-side commands.
+    /// </summary>
+    public interface IClientCommand : ICommand {
+        /// <summary>
+        /// Executes the command with the given arguments.
+        /// </summary>
+        /// <param name="arguments">A string array containing the arguments for this command. The first argument
+        /// is the command trigger or alias.</param>
+        void Execute(string[] arguments);
+    }
+}
---
+++ HKMP/Api/Command/Client/IClientCommandManager.cs
@@ -0,0 +1,7 @@
+namespace Hkmp.Api.Command.Client {
+    /// <summary>
+    /// Interface for managing commands for client-side.
+    /// </summary>
+    public interface IClientCommandManager : ICommandManager<IClientCommand> {
+    }
+}
