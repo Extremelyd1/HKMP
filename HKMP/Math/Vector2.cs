19dfa582d40533a1cca2fa9f40290e07cd6403e4
Extremelyd1 <Extremelyd1@outlook.com>
2022-03-10 19:27:56 +0100

    Redo access modifiers and documentation for Hkmp.Math namespace

--- HKMP/Math/Vector2.cs
+++ HKMP/Math/Vector2.cs
@@ -1,15 +1,33 @@
 namespace Hkmp.Math {
+    /// <summary>
+    /// Class for two dimensional vectors.
+    /// </summary>
     public class Vector2 {
+        /// <summary>
+        /// The zero (0, 0) vector.
+        /// </summary>
         public static readonly Vector2 Zero = new Vector2(0, 0);
 
+        /// <summary>
+        /// The X coordinate of this vector.
+        /// </summary>
         public float X { get; set; }
+        /// <summary>
+        /// The Y coordinate of this vector.
+        /// </summary>
         public float Y { get; set; }
 
+        /// <summary>
+        /// Construct a vector with the given X and Y values.
+        /// </summary>
+        /// <param name="x">The X coordinate.</param>
+        /// <param name="y">The Y coordinate.</param>
         public Vector2(float x, float y) {
             X = x;
             Y = y;
         }
 
+        /// <inheritdoc />
         public override bool Equals(object obj) {
             if (!(obj is Vector2 vector2)) {
                 return false;
@@ -18,16 +36,28 @@ namespace Hkmp.Math {
             return Equals(vector2);
         }
 
+        /// <summary>
+        /// Determines whether the given vector is equal to the current instance.
+        /// </summary>
+        /// <param name="other">The vector to compare with the current vector.</param>
+        /// <returns>true if the given vector is equal to the current object; otherwise, false.</returns>
         private bool Equals(Vector2 other) {
             return X.Equals(other.X) && Y.Equals(other.Y);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode() {
             unchecked {
                 return (X.GetHashCode() * 397) ^ Y.GetHashCode();
             }
         }
-        
+
+        /// <summary>
+        /// Determines whether the given vectors are equal.
+        /// </summary>
+        /// <param name="lhs">The left-hand side to compare.</param>
+        /// <param name="rhs">The right-hand side to compare.</param>
+        /// <returns>true if the given vectors are equal; otherwise, false.</returns>
         public static bool operator ==(Vector2 lhs, Vector2 rhs) {
             if ((object) lhs == null) {
                 return (object) rhs == null;
@@ -35,7 +65,13 @@ namespace Hkmp.Math {
             
             return lhs.Equals(rhs);
         }
-        
+
+        /// <summary>
+        /// Determines whether the given vector are not equal.
+        /// </summary>
+        /// <param name="lhs">The left-hand side to compare.</param>
+        /// <param name="rhs">The right-hand side to compare.</param>
+        /// <returns>true if the given vectors are not equal; otherwise, false.</returns>
         public static bool operator !=(Vector2 lhs, Vector2 rhs) {
             return !(lhs == rhs);
         }
--- HKMP/Math/Vector3.cs
+++ HKMP/Math/Vector3.cs
@@ -1,9 +1,27 @@
 namespace Hkmp.Math {
+    /// <summary>
+    /// Class for three dimensional vectors.
+    /// </summary>
     public class Vector3 {
+        /// <summary>
+        /// The X coordinate of this vector.
+        /// </summary>
         public float X { get; set; }
+        /// <summary>
+        /// The Y coordinate of this vector.
+        /// </summary>
         public float Y { get; set; }
+        /// <summary>
+        /// The Z coordinate of this vector.
+        /// </summary>
         public float Z { get; set; }
 
+        /// <summary>
+        /// Construct a vector with the given X, Y and Z values.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public Vector3(float x, float y, float z) {
             X = x;
             Y = y;
