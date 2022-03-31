ac1d7540c3bad88c41a3693e9abecb471d07f394
Extremelyd1 <Extremelyd1@outlook.com>
2022-03-31 16:44:26 +0200

    Change assembly info of projects

--- HKMPServer/Properties/AssemblyInfo.cs
+++ HKMPServer/Properties/AssemblyInfo.cs
@@ -2,35 +2,7 @@
 using System.Runtime.InteropServices;
 using Hkmp;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("HKMPServer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HKMPServer")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5AB0E450-3F37-4715-916F-CF2EC62D398B")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(Version.String)]
 [assembly: AssemblyFileVersion(Version.String)]
