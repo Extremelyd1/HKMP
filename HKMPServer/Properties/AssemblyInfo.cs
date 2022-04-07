afc7abd05a540e7fd4d07ca7b7fe12af301f4507
Extremelyd1 <Extremelyd1@outlook.com>
2022-04-07 20:45:51 +0200

    Update project info files to better format

--- HKMPServer/Properties/AssemblyInfo.cs
+++ HKMPServer/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using Hkmp;
+﻿using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HKMPServer")]
 [assembly: Guid("5AB0E450-3F37-4715-916F-CF2EC62D398B")]
-[assembly: AssemblyVersion(Version.String)]
-[assembly: AssemblyFileVersion(Version.String)]
