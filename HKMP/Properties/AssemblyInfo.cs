6f814aaa2942d7f1005a8f0f2d32087521099fa2
Extremelyd1 <Extremelyd1@outlook.com>
2022-04-07 20:53:49 +0200

    Add Github Actions (#52)
    * Change project files to github actions
    
    * Initial dotnet workflow
    
    * Include project directory in dotnet cmds
    
    * Add list files to workflow
    
    * Add solution file to root folder
    
    * Update project info files to better format
    
    * Change output type of server to exe

--- HKMP/Properties/AssemblyInfo.cs
+++ HKMP/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HKMP")]
 [assembly: Guid("F34118B2-515D-4C33-88E6-9CFEF2AD5A15")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: InternalsVisibleTo("HKMPServer")]
