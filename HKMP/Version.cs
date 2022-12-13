5a4503461d49f88804e9a273230bbd3b5d802dd4
Extremelyd1 <Extremelyd1@outlook.com>
2022-12-13 20:26:44 +0100

    Bump version number

--- HKMP/Version.cs
+++ HKMP/Version.cs
@@ -6,6 +6,6 @@ namespace Hkmp {
         /// <summary>
         /// The version as a string.
         /// </summary>
-        public const string String = "2.2.2";
+        public const string String = "2.2.3";
     }
 }
