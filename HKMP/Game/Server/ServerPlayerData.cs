bd5fe8fd6b400a38afb8cca76bbf841ab58eea88
Extremelyd1 <Extremelyd1@outlook.com>
2023-02-03 12:44:16 +0100

    Fix concurrency with client scene data

--- HKMP/Game/Server/ServerPlayerData.cs
+++ HKMP/Game/Server/ServerPlayerData.cs
@@ -73,6 +73,8 @@ namespace Hkmp.Game.Server {
             Username = username;
             AuthKey = authKey;
 
+            CurrentScene = "";
+
             _authorizedList = authorizedList;
         }
     }
