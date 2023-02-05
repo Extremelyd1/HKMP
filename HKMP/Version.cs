790adc53767176854d7adc856bb8a59077ec8eb7
Extremelyd1 <Extremelyd1@outlook.com>
2023-02-05 14:07:43 +0100

    Bump version number

--- HKMP/Version.cs
+++ HKMP/Version.cs
@@ -7,5 +7,5 @@ internal static class Version {
     /// <summary>
     /// The version as a string.
     /// </summary>
-    public const string String = "2.2.3";
+    public const string String = "2.2.4";
 }
