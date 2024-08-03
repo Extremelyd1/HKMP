92cdc2db2a5236fd098850d1cf2e431f9606d2e0
Extremelyd1 <10898310+Extremelyd1@users.noreply.github.com>
2024-08-03 15:31:40 +0200

    Bump version number

--- HKMP/Version.cs
+++ HKMP/Version.cs
@@ -7,5 +7,5 @@ internal static class Version {
     /// <summary>
     /// The version as a string.
     /// </summary>
-    public const string String = "2.4.1-es";
+    public const string String = "3.0.0-es";
 }
