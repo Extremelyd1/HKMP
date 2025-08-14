f748587d8d0e302089cbbb7bd18a27a5ec1a75da
dplochcoder <daniel.ploch.coder@gmail.com>
2025-08-13 20:26:44 -0400

    Allow loading multiple addons from a single dll

--- HKMP/Api/Addon/AddonLoader.cs
+++ HKMP/Api/Addon/AddonLoader.cs
@@ -96,8 +96,6 @@ internal abstract class AddonLoader {
                 }
 
                 addons.Add(addon);
-                // We only allow a single class extending the addon subclass
-                break;
             }
         }
 
